733fdfc96d74516ce803dd0ca5ae82cfae70b1b7
vishwajeetsingh11 <64366781+vishwajeetsingh11@users.noreply.github.com>
2024-10-03 19:18:04 +0530

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# Change to the app directory
+# Set variables
+ECR_IMAGE="950196620421.dkr.ecr.us-east-1.amazonaws.com/python:latest"
+CONTAINER_NAME="my-flask-app"
 
 # Authenticate Docker to ECR
-aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 950196620421.dkr.ecr.us-east-1.amazonaws.com/python:latest
+aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin ${ECR_IMAGE}
 
 # Pull the Docker image
-sudo docker pull 950196620421.dkr.ecr.us-east-1.amazonaws.com/python:latest
+sudo docker pull ${ECR_IMAGE}
 
 # Stop and remove any existing container
-if [ "$(sudo docker ps -q -f name=my-flask-app)" ]; then
-    sudo docker stop my-flask-app
-    sudo docker rm my-flask-app
+if [ "$(sudo docker ps -q -f name=${CONTAINER_NAME})" ]; then
+    echo "Stopping existing container ${CONTAINER_NAME}..."
+    sudo docker stop ${CONTAINER_NAME}
+fi
+
+# Remove the container regardless of whether it was running or not
+if [ "$(sudo docker ps -aq -f name=${CONTAINER_NAME})" ]; then
+    echo "Removing container ${CONTAINER_NAME}..."
+    sudo docker rm ${CONTAINER_NAME}
 fi
 
 # Run the Docker container
-sudo docker run -d -p 5000:5000 --name my-flask-app 950196620421.dkr.ecr.us-east-1.amazonaws.com/python:latest
+echo "Starting new container ${CONTAINER_NAME}..."
+sudo docker run -d -p 5000:5000 --name ${CONTAINER_NAME} ${ECR_IMAGE}

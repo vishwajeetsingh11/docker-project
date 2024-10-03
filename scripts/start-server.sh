0e2d0bffab55db5e2e944cf9c13d4f0cb097f99d
vishwajeetsingh11 <64366781+vishwajeetsingh11@users.noreply.github.com>
2024-10-03 18:57:48 +0530

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 # Change to the app directory
-cd /home/ec2-user/my-flask-app
+cd /home/ec2-user/my-flask-app || exit
 
-# Build the Docker image
-sudo docker build -t my-flask-app .
+# Authenticate Docker to ECR
+aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 950196620421.dkr.ecr.us-east-1.amazonaws.com/python:latest
+
+# Pull the Docker image
+sudo docker pull 950196620421.dkr.ecr.us-east-1.amazonaws.com/python:latest:latest
+
+# Stop and remove any existing container
+if [ "$(sudo docker ps -q -f name=my-flask-app)" ]; then
+    sudo docker stop my-flask-app
+    sudo docker rm my-flask-app
+fi
 
 # Run the Docker container
-sudo docker run -d -p 5000:5000 --name my-flask-app my-flask-app
-
+sudo docker run -d -p 5000:5000 --name my-flask-app 950196620421.dkr.ecr.us-east-1.amazonaws.com/python:latest
